3329ffc99857aeb496733379d1ff6d0d8dc93ad8
naughtyStark <sidharthtalia@ymail.com>
2022-07-05 15:18:55 +0530

    all launch files put inside hound_core

--- setup/initenv.sh
+++ setup/initenv.sh
@@ -1,9 +1,7 @@
 #!/bin/bash
-echo  'KERNEL=="ttyUSB*", ATTRS{idVendor}=="10c4", ATTRS{devnum}=="4", MODE:="0666", GROUP:="dialout",  SYMLINK+="ydlidar"' >/etc/udev/rules.d/ydlidar.rules
+echo  'KERNEL=="ttyUSB*", KERNELS=="1-1.3:1.0", ATTRS{idVendor}=="10c4", MODE:="0666", GROUP:="dialout",  SYMLINK+="ydlidar"' >/etc/udev/rules.d/ydlidar.rules
 
-echo  'KERNEL=="ttyACM*", ATTRS{idVendor}=="0483", ATTRS{idProduct}=="5740", MODE:="0666", GROUP:="dialout",  SYMLINK+="ardupilot"' >/etc/udev/rules.d/ardupilot-usb.rules
-
-echo  'KERNEL=="ttyUSB*", ATTRS{idVendor}=="10c4", ATTRS{devnum}=="3", MODE:="0666", GROUP:="dialout",  SYMLINK+="ardupilot"' >/etc/udev/rules.d/ardupilot.rules
+echo  'KERNEL=="ttyUSB*", KERNELS=="1-1.4:1.0", ATTRS{idVendor}=="10c4", MODE:="0666", GROUP:="dialout",  SYMLINK+="ardupilot"' >/etc/udev/rules.d/ardupilot.rules
 
 service udev reload
 sleep 2
