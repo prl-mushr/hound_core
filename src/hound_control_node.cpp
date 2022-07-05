5159cb53688cb801e51bbe1230657e4dea5baf99
naughtyStark <sidharthtalia@ymail.com>
2022-07-05 15:39:34 +0530

    one for all sensors

--- src/hound_control_node.cpp
+++ src/hound_control_node.cpp
@@ -1,16 +1,24 @@
 #include <ros/ros.h>
 
+
+#include <message_filters/subscriber.h>
+#include <message_filters/time_synchronizer.h>
+#include <message_filters/synchronizer.h>
+#include <message_filters/sync_policies/approximate_time.h>
 #include <opencv2/opencv.hpp>
-// Include opencv2
+// Include opencv2 -- why? because I need something to take care of the matrix and vector stuff.
+// I'm really just using opencv as a replacement for numpy in C++. could have used eigen but why switch
 #include <opencv2/imgproc/imgproc.hpp>
 #include <opencv2/highgui/highgui.hpp>
 #include "geometry_msgs/PoseStamped.h"
 #include "geometry_msgs/PoseArray.h"
 #include "geometry_msgs/Quaternion.h"
 #include "nav_msgs/Odometry.h"
+#include "mavros_msgs/RCOut.h"
 #include "sensor_msgs/Imu.h"
 #include "ackermann_msgs/AckermannDriveStamped.h"
 
+using namespace message_filters;
 
 class Bezier
 {
@@ -93,7 +101,7 @@ public:
     return cv::norm(P[3] - goal);
   }
 
-  float get_CTN(float t, float &C, cv::Point3f &Tan, cv::Point3f &Norm)
+  void get_CTN(float t, float &C, cv::Point3f &Tan, cv::Point3f &Norm)
   {
     cv::Point3f B1, B2, BNormal;
     float k_num, k_den;
@@ -121,17 +129,29 @@ public:
     return P[0]*t_[0] + P[1]*t_[1] + P[2]*t_[2] + P[3]*t_[3];
   }
 
-  float step_size(float resolution)
+  float get_T(float dist)
   {
-    return resolution/arc_length;
+    return dist/arc_length;
   }
 };
 
 class Juicebox
 {
+private:
+  cv::Point3f P0,P3;
+
 public:
-  ros::Subscriber sub_odom, sub_imu;
+  message_filters::Subscriber<sensor_msgs::Imu> imu_sub;
+  message_filters::Subscriber<nav_msgs::Odometry> odom_sub;
+  message_filters::Subscriber<mavros_msgs::RCOut> rc_sub;
+
   ros::Publisher control_publisher;
+
+  typedef sync_policies::ApproximateTime<sensor_msgs::Imu, nav_msgs::Odometry, mavros_msgs::RCOut> MySyncPolicy;
+  typedef Synchronizer<MySyncPolicy> Sync;
+  boost::shared_ptr<Sync> sync;
+
+
   // model variables
   float D_const, B_const, C_const, wheelbase, trackwidth, cg_height, cg_ratio;
   float steering_limit, mass, wheelspeed_lim;
@@ -139,27 +159,48 @@ public:
   // state variables. ENU for world frame, FLU for body frame:
   float rpy[3];
   geometry_msgs::Quaternion Q;
-  cv::Point3f pos, vel, lastVel, velBF, accBF, accNED, rotBF, phi_dot;
+  cv::Point3f pos, vel, lastVel, velBF, accBF, accNED, rotBF, phi_dot, accelVelBFFrame;
   float Beta, speed;
   cv::Matx33f Tnb, Tbn;
-
-
   // control variables
   cv::Point3f Vhat_cur;
   float wheelspeed, steering, steering_estimate, wheelspeed_estimate;
+  float throttle_out, steering_out;
   cv::Point3f target_WP, cur_target, cur_Vhat, target_Vhat;
   float control_Tc, speed_time_constant, wp_dist;
   bool auto_flag, setup_complete;
   int wp_index, max_index;
 
+  float error[1000];
+
   Juicebox(ros::NodeHandle &nh)
   {
     Tnb = cv::Matx33f(0,0,0, 0,0,0, 0,0,0);
     Tbn = cv::Matx33f(0,0,0, 0,0,0, 0,0,0);
+    P0 = cv::Point3f(0,0,0);
+    control_Tc = 0.02f;
+    odom_sub.subscribe(nh, "odom", 2);
+    imu_sub.subscribe(nh, "imu", 2);
+    rc_sub.subscribe(nh, "rcout", 2);
 
-    sub_odom = nh.subscribe("odom", 1, &Juicebox::odom_cb, this);
-    sub_imu = nh.subscribe("imu", 1, &Juicebox::imu_cb, this);
+    sync.reset(new Sync(MySyncPolicy(10), imu_sub, odom_sub, rc_sub));   
+    sync->registerCallback(boost::bind(&Juicebox::state_cb, this, _1, _2,_3));
+
     control_publisher = nh.advertise<ackermann_msgs::AckermannDriveStamped>("control",10);
+
+    for(int i = 0;i < 1000; i++)
+    {
+      error[i] = 0;
+    }
+
+    wheelbase = 0.29f;
+    D_const = 1.2f;
+    C_const = 1.4f;
+    B_const = 6.5f;
+    mass = 3.0f;
+    cg_height = 0.1;
+    throttle_out = 0;
+
     ROS_INFO("initialized");
   }
 
@@ -227,51 +268,79 @@ public:
 
   float constrain(float val, float min, float max)
   {
-    if(val > max)
+    if(val >= max)
     {
       return max;
     }
-    if(val < min);
+    if(val <= min)
     {
       return min;
     }
     return val;
   }
 
-  void imu_cb(const sensor_msgs::Imu::ConstPtr msg)
+  cv::Point3f forward_vector()
   {
-    accBF.x = msg->linear_acceleration.x;
-    accBF.y = msg->linear_acceleration.y;
-    accBF.z = msg->linear_acceleration.z;
+    cv::Point3f W;
+    W.x = 1 - 2 * (Q.y*Q.y + Q.z*Q.z);
+    W.y = 2 * (Q.x*Q.y + Q.w*Q.z);
+    W.z = 2 * (Q.x*Q.z - Q.w*Q.y);
+    return W;
   }
 
-  void odom_cb(const nav_msgs::Odometry::ConstPtr& msg)
+  cv::Point3f rotate(cv::Point3f vec, float angle)
   {
-    pos.x = msg->pose.pose.position.y;
-    pos.y = msg->pose.pose.position.x;
+    cv::Point3f out;
+    out.x = vec.x * cosf(angle) - vec.y * sinf(angle);
+    out.y = vec.x * sinf(angle) + vec.y * cosf(angle);
+    out.z = vec.z;
+    return out;
+  }
+
+  void state_cb(const sensor_msgs::Imu::ConstPtr imu, const nav_msgs::Odometry::ConstPtr& msg, const mavros_msgs::RCOut::ConstPtr rc)
+  {
+    pos.x = msg->pose.pose.position.x;
+    pos.y = msg->pose.pose.position.y;
     pos.z = msg->pose.pose.position.z;
 
-    velBF.x = msg->twist.twist.linear.y;
-    velBF.y = msg->twist.twist.linear.x;
+    velBF.x = msg->twist.twist.linear.x;
+    velBF.y = msg->twist.twist.linear.y;
     velBF.z = msg->twist.twist.linear.z;
 
-    rotBF.x = msg->twist.twist.angular.y;
-    rotBF.y = msg->twist.twist.angular.x;
+    rotBF.x = msg->twist.twist.angular.x;
+    rotBF.y = msg->twist.twist.angular.y;
     rotBF.z = msg->twist.twist.angular.z;
+
+    cv::Point3f temp_acc;
+    temp_acc.x = imu->linear_acceleration.x;
+    temp_acc.y = imu->linear_acceleration.y;
+    temp_acc.z = imu->linear_acceleration.z;
+
+    accBF = LPF(0.2, temp_acc, accBF);
+
+    throttle_out = LPF(0.5, 14.28f*(rc->channels[2] - 1050.0f)/850.0f, throttle_out);
+    steering_out = 0.488f*(rc->channels[3] - 1500.0f)/250.0f;
 
     Q = msg->pose.pose.orientation;
     calc_Transform(Q, Tnb, Tbn);
     rpy_from_quat(rpy);
 
     lastVel = vel;
-    vel = Tnb * velBF;
+    vel = Tbn * velBF;
 
     speed = cv::norm(velBF);
-    Beta = LPF(0.2f, atan2f(velBF.y, velBF.x), Beta);
+    if(fabs(velBF.y) < 0.1 and fabs(velBF.x) < 0.1)
+    {
+      Beta = 0;
+    }
+    else
+    {
+      Beta = LPF(0.2f, atan2f(velBF.y, velBF.x), Beta);
+    }
     if(speed > 1)
     {
       cv::Point3f past_vec = Tnb * lastVel / cv::norm(lastVel);
-      cv::Point3f cur_vec = vel/ speed;
+      cv::Point3f cur_vec = velBF/ speed;
       cv::Point3f rot_vec = past_vec.cross(cur_vec);
       if(cv::norm(rot_vec)!=0)
       {  
@@ -281,20 +350,176 @@ public:
       {
           rot_vec = cv::Point3f(0,0,1);
       }
-      phi_dot = LPF(0.2, 50*rot_vec * acosf( constrain(past_vec.dot(cur_vec), -1.0, 1.0) ), phi_dot);
+      phi_dot = LPF(0.2, 50*rot_vec * acosf( constrain(past_vec.dot(cur_vec), -1.0f, 1.0f )), phi_dot);
     }
     else
     {
       phi_dot = rotBF;
     }
 
-    std::cout<<"pos: "<<pos<<std::endl;
-    std::cout<<"vel: "<<vel<<std::endl;
-    std::cout<<"velBF: "<<velBF<<std::endl;
-    std::cout<<"rpy: "<<rpy[2]*57.3<<std::endl;
-    std::cout<<"phi_dot: "<<phi_dot.z*57.3<<std::endl;
-    std::cout<<"Beta "<<Beta<<std::endl;
+    accelVelBFFrame = rotate(accBF, Beta);
+
+    // std::cout<<"pos m: "<<pos<<std::endl;
+    // std::cout<<"vel m/s: "<<vel<<std::endl;
+    std::cout<<"accBF m/ss: "<<sqrtf(fabs(cv::norm(accBF)*cv::norm(accBF) - 9.8*9.8))<<std::endl;
+    // std::cout<<"velBF m/s: "<<velBF<<std::endl;
+    // std::cout<<"rpy deg: "<<rpy[2]*57.3<<std::endl;
+    // std::cout<<"phi_dot deg/s: "<<phi_dot.z*57.3<<std::endl;
+    // std::cout<<"Beta deg :"<<Beta*57.3<<std::endl;
+    // std::cout<<"wheelspeed m/s: "<<throttle_out<<std::endl;
+    // std::cout<<"steering deg:" <<57.3 * steering_out<<std::endl;
+
+    Tire_model_prediction();
   }
+
+  void tire_model(float W, float Vbody, float wheelspeed, float Beta, float angle, float side,
+                  float &Force, float &gamma, float &slip, float D, float C, float B)
+  {
+    float Vx, Vy, sx, sy;
+    Vx = Vbody*cosf(Beta - angle);
+    Vy = Vbody*sinf(Beta - angle);
+    sx = (wheelspeed - Vx)/std::max(1.0f, std::max(wheelspeed, Vx));  // prevent 0/0 cases.
+    sy = (Vy + side*wheelbase*0.5f*(rotBF.z))/ std::max(wheelspeed,1.0f);  // prevent 0/0 case
+    slip = sqrtf(sx*sx + sy*sy);
+    gamma = atan2f(-sy, sx);
+    Force = W * D * sinf( C * atanf( B * slip));
+  }
+
+  cv::Point3f calc_forces(float Wf, float Wr, float speed, float Beta, float steering, float wheelspeed, float D, float C, float B)
+  {
+    float m_inv = 1.0f/mass;
+    float Ff, Fr, gf, gr;
+    float th_f, th_r;
+    float sf, sr;
+    cv::Point3f AccPred;
+    tire_model(Wf, speed, wheelspeed, Beta, steering, 1, Ff, gf, sf, D, C, B);
+    tire_model(Wr, speed, wheelspeed, Beta, 0,       -1, Fr, gr, sr, D, C, B);  // rear tires have no steering angle.
+    
+    th_f = steering - (Beta - gf);
+    th_r = gr - Beta;
+
+    AccPred.x = m_inv *( Ff*cosf(th_f) + Fr*cosf(th_r) );
+    AccPred.y = m_inv *( Ff*sinf(th_f) + Fr*sinf(th_r) );
+    AccPred.z = accelVelBFFrame.z;
+
+    return AccPred;
+  }
+
+  void Tire_model_prediction()
+  {
+    float Wf, Wr;
+    float Wb2 = mass * 9.8f * 0.5f;
+    float Lt = (2 * accBF.x * mass * cg_height) / wheelbase;
+    cv::Point3f AccPred;
+    Wr = Wb2 + Lt;
+    Wf = Wb2 - Lt;
+    if(speed < 2)
+    {
+      return;
+    }
+    int index;
+    float min_error = 1e7;
+    float D_test, C_test, B_test;
+
+    float spread = 1;//cv::norm(accelVelBFFrame - calc_forces(Wf, Wr, speed, Beta, steering_out, throttle_out, D_test, C_test, B_test) );
+    float D_spread = spread;
+    float C_spread = spread;
+    float B_spread = 6*spread;
+    float D_mean = 1.0, C_mean = 1.5, B_mean = 4.0;
+
+    ros::Time start = ros::Time::now();
+
+    for(int i = 0; i < 10; i++)
+    {
+      D_test = (D_mean - 0.5f*D_spread) + 0.1f*D_spread*i;
+      D_test = constrain(D_test, 0.5, 1.5);
+      for(int j = 0; j < 10; j++)
+      {
+        C_test = (C_mean - 0.5f*C_spread) + 0.1f*C_spread*j;
+        C_test = constrain(C_test, 1.0, 2.0);
+
+        for(int k = 0; k < 100; k++)
+        {
+          B_test = (B_mean - 0.5f*B_spread) + 0.01f*B_spread*k;
+          B_test = constrain(B_test, 1.0, 7.0);
+
+          index = 100*i + 10*j + k;
+          AccPred = calc_forces(Wf, Wr, speed, Beta, steering_out, throttle_out, D_test, C_test, B_test);
+          error[index] *= 0.99;
+          error[index] += cv::norm(AccPred - accelVelBFFrame);
+          if(error[index] < min_error)
+          {
+            min_error = error[index];
+            D_const = D_test;
+            C_const = C_test;
+            B_const = B_test;
+          }
+        }
+      }
+    }
+    
+    AccPred = calc_forces(Wf, Wr, speed, Beta, steering_out, throttle_out, D_const, C_const, B_const);
+    std::cout<<"predicted: "<<AccPred<<std::endl;
+    std::cout<<"measured: "<<accelVelBFFrame << std::endl;
+    std::cout<<"DCB : ";
+    std::cout<<D_const<<"||";
+    std::cout<<C_const<<"||";
+    std::cout<<B_const<<"||";
+    std::cout<<std::endl;
+    return;
+  }
+
+
+  void pub_cur_goal()
+  {
+    return;
+  }
+
+  void publish_controls()
+  {
+    return;
+  }
+
+  // void controller()
+  // {
+  //   P3 = pos_target - pos;
+
+  //   if(not auto_flag)
+  //   {
+  //     wheelspeed = 0;
+  //     steering = 0;
+  //     publish_controls();
+  //   }
+
+  //   if(speed < 0.5f)
+  //   {
+  //     Vhat_cur = forward_vector();
+  //   }
+  //   else
+  //   {
+  //     Vhat_cur = vel/speed;
+  //   }
+  //   pub_cur_goal(); // publish current goal. TBD
+
+  //   Vhat_final = vel_target/cv::norm(vel_target); // what is vel target?
+
+  //   Bezier traj(P0, Vhat_cur, Vhat_final, P3, true);
+  //   float t = traj.get_T(speed * control_Tc);
+
+  //   float Curvature, C_max;
+  //   cv::Point3f Direction,Normal;
+  //   traj.get_CTN(t, Curvature, Direction, Normal);
+  //   Normal = Tnb * Normal;
+  //   Direction = Tnb * Direction;
+
+  //   C_max = std::max(traj.C[0], traj.C[1]);
+
+  //  /// the following code is specific to cars, sort of like inverse kinematics
+  //   cv::Point3f C = Curvature * Normal;
+  //   float horizontal_curvature = C[1];
+  //   // get the velocity in body frame, rotate normal vector into vel frame then find phi dot.
+  //   float phi_dot_setp = horizontal_curvature*speed
+  // }
 
 
 };
