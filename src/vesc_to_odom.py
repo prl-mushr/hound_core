4b7b4a8d76feb5d72a3b2765a192f09d610abf0b
naughtyStark <sidharthtalia@ymail.com>
2022-07-14 03:36:26 -0400

    vesc to odom added

--- src/vesc_to_odom.py
+++ src/vesc_to_odom.py
@@ -1,15 +1,29 @@
-#!/usr/bin/env python3
+#!/usr/bin/env python
 # Import ROS libraries and messages
 import rospy
-from vesc_msgs.msgs import VescStateStamped
+from vesc_msgs.msg import VescStateStamped
 from nav_msgs.msg import Odometry
+import time
+
+rospy.init_node('vesc_to_odom_node', anonymous=True)
+
+last_callback_time = time.time()
 
 def translate_cb(msg):
-	speed = msg.speed
+        global last_callback_time
+	speed = msg.state.speed
 	odom = Odometry()
-	odom.twist.twist.linear.x = speed
-	odom_pub.publish(odom)
+        odom.header.stamp = rospy.Time.now()
+	odom.twist.twist.linear.x = speed/3500
+        odom.twist.covariance[0] = max(0.01*speed/3500,0.1)
+        if(time.time() - last_callback_time > 0.1):
+	        odom_pub.publish(odom)
+                print("hit")
+                last_callback_time = time.time()
 
 
 odom_pub = rospy.Publisher("/mavros/vision_pose/vis_odom", Odometry, queue_size = 1)
 vesc_sub = rospy.Subscriber("sensors/core", VescStateStamped, translate_cb, queue_size = 1)
+
+while not rospy.is_shutdown():
+  time.sleep(0.1)
