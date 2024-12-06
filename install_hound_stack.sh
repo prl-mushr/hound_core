9a62d40d277fda1ba9fa0d6ab0db2c8cd97d5ac4
Amy Le <115192175+anhle5@users.noreply.github.com>
2024-12-06 14:57:40 -0800

    Create install_hound_stack.sh

---
+++ install_hound_stack.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+echo "Installing Docker..."
+sudo apt-get update
+sudo apt-get install ca-certificates curl
+sudo install -m 0755 -d /etc/apt/keyrings
+sudo curl -fsSL https://download.docker.com/linux/ubuntu/gpg -o /etc/apt/keyrings/docker.asc
+sudo chmod a+r /etc/apt/keyrings/docker.asc
+echo \
+  "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.asc] https://download.docker.com/linux/ubuntu \
+  $(. /etc/os-release && echo "$VERSION_CODENAME") stable" | \
+  sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+sudo apt-get update
+sudo apt-get install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
+
+echo "Installing Nvidia Docker..."
+curl -fsSL https://nvidia.github.io/libnvidia-container/gpgkey | sudo gpg --dearmor -o /usr/share/keyrings/nvidia-container-toolkit-keyring.gpg \
+  && curl -s -L https://nvidia.github.io/libnvidia-container/stable/deb/nvidia-container-toolkit.list | \
+    sed 's#deb https://#deb [signed-by=/usr/share/keyrings/nvidia-container-toolkit-keyring.gpg] https://#g' | \
+    sudo tee /etc/apt/sources.list.d/nvidia-container-toolkit.list
+sudo apt-get update
+sudo apt-get install -y nvidia-container-toolkit
+
+echo "Installing HOUND Stack..."
+mkdir catkin_ws && cd catkin_ws && mkdir src && cd src
+git clone -b noetic-HOUND https://github.com/prl-mushr/mushr
+cd ~/catkin_ws/src/mushr/mushr_utils/install/
+echo -e "y\ny\nn" | ./mushr_install.bash
+sudo reboot
